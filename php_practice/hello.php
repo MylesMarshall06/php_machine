57e8ab4e3d00d193c3fc99eecf9e062ad0d6b75f
Myles Marshall <142362617+MylesMarshall06@users.noreply.github.com>
2025-01-10 00:28:49 +0000

    end of jan 9

---
+++ php_practice/hello.php
@@ -0,0 +1,9 @@
+<?php
+$myName = "Myles";
+$myFriend = "Benadryl";
+
+echo "Hello, world. \n";
+echo "My name is $myName. \n";
+echo "My friend is $myFriend. \n";
+echo "Hello $myFriend, " . "my name is $myName. \n";
+?>
